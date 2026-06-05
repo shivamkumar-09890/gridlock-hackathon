d8530f8d79a60ea7fd762ec379db02ffded1925a
shivamkumar-09890 <shivamkumar878758@gmail.com>
2026-06-05 16:23:26 +0530

    feat: building XGBoost

---
+++ src/models/xgboost_model.py
@@ -0,0 +1,27 @@
+from xgboost import XGBRegressor
+
+
+class XGBoostDemandModel:
+
+    def __init__(self):
+
+        self.params = {
+            "n_estimators": 5000,
+            "learning_rate": 0.02,
+            "max_depth": 10,
+            "subsample": 0.8,
+            "colsample_bytree": 0.8,
+            "objective": "reg:squarederror",
+            "eval_metric": "rmse",
+            "random_state": 42,
+
+            # Important
+            "enable_categorical": True,
+            "tree_method": "hist",
+        }
+
+    def build(self):
+
+        return XGBRegressor(
+            **self.params
+        )
